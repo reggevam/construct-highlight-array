c5038497450a2eb79dfe95ed23e28230167ba207
reggeva <barak.eden@devalore.com>
2019-05-21 10:49:26 +0300

    Added tests and comments and jsodc annotation

---
+++ __tests__/mapEntitiesToRenderArray.js
@@ -0,0 +1,33 @@
+const mapEntitiesToRenderArray = require('./../src');
+const smallText = require('../dummy-data/raw-text-small');
+const smallLabels = require('../dummy-data/labels-small.js');
+
+const largeText = require('../dummy-data/raw-text');
+const largeLabels = require('../dummy-data/labels.js');
+
+describe('mapEntitiesToRenderArray', () => {
+  test('adds empty label tags to from raw text to highligh array on small version', () => {
+    const result = mapEntitiesToRenderArray(smallText, smallLabels);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('adds empty label tags to from raw text to highligh array on large version', () => {
+    const result = mapEntitiesToRenderArray(largeText, largeLabels);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('expects each odd entity to be labeled and each even entity to be unlabeled', () => {
+    const result = mapEntitiesToRenderArray(largeText, largeLabels);
+
+    const labeled = result.filter((_, ii) => ii % 2);
+    const unlabeled = result.filter((_, ii) => ii % 2 === 0);
+
+    labeled.forEach(entity => {
+      expect(entity.isEntity).toBeTruthy();
+    });
+
+    unlabeled.forEach(entity => {
+      expect(entity.isEntity).not.toBeTruthy();
+    });
+  });
+});
---
+++ dummy-data/raw-text.js
@@ -0,0 +1,18 @@
+module.exports = `The following is a statement of Patrolman Mark Coral with reference to the investigation of the homicide of Ana Smith age 31, married to Mark Smith.
+On the morning of Sunday 01/01/1970, I was dispatched to 1600 Pennsylvania Avenue Northwest, Washington, DC, USA due to a call regarding a dead body.
+Upon arrival of officer Margaret Heinz and myself, we met with Mark Smith the husband of the deceased. Mark Smith met us at the entrance to the 4 story building. There is no elevator in the building, one stair-way and no external fire escape. 
+Before entering the building I noticed a city street camera right in front of the building.
+The apartment is located on the second floor. Before entering, I asked Mr. Smith to tell me what had happened. Mr. Smith was very anxious and distressed. 
+According to him, he left his residence last night at around 20:00 for his night shift and returned at 08:00. He works as an operations dispatcher for the GAR � Garbage Company. He said he was surprised to find the door unlocked and slightly opened as his wife was supposed to be at work already. He entered the apartment and called for his wife. As he entered the living room he found Ana on the floor with a head injury and a pool of blood around her. He ran out immediately to call the police. I asked Mr. Smith if he had touched the body or anything else and he said he did not. 
+Mr. Smith's behavior seemed odd. I asked him when was the last time he was in contact with his wife and he replied it was the previous evening when he left for work. He tried calling her at 06:00 but she didn't answer.
+No apparent forced entry was noticed. The body was on the floor with a head injury and a gun beside it. The apartment was very clean and neat. No note was found in the apartment. Ana was fully dressed and our initial examination of the scene did not show any signs of burglary or assault. According to Mr. Smith, Ana started her day at 05:30 every morning. She was a sales person at MACY's and has a two hour commute. 
+A laptop and cellphone belonging to the deceased were filed into evidence.
+ 
+The guns serial number found was BT17566
+According to Mr. Smith they do not own a gun.
+
+I proceeded to question the neighbors living in the building.
+The first floor neighbor is Mr. Jacob Prat, 95 years old. He did not hear anything or see anything out of the ordinary. He was asleep at 20:00 and woke up at 07:00.
+The third floor neighbors are the Swan Family. Adam age 45 and his wife Terry, they have a 5 year old son � Patrick that was at his grandmothers. According to Adam and Terry they were  woken up at 05:30 by a loud bang that sounded like a car back fire. Following shortly they heard a car race off in the street adjacent to the building. They went back to sleep.
+The Swans had no real acquaintance with the Smiths but did say that they were always shouting at each other.  
+The fourth floor neighbor is Simon Zeus a 29 year old student. He too was awoken by a loud noise but according to him the time was 04:30.`;
